edee5ce9ade9ccd61762abb84550b0d9e6fa4ab9
krishna306 <krishnakumarkeer5017@gmail.com>
2022-07-16 19:30:33 +0530

    Update connection

--- connection.js
+++ connection.js
@@ -1,19 +1,3 @@
-// const mongoose = require("mongoose");
-// require('dotenv').config()
-// mongoose.connect("mongodb://localhost:27017/blogpost")
-// .then(console.log("connected to Database"))
-// .catch((err) => console.log(err));
-
-// // const uri = process.env.MongoURL;
-// // mongoose.connect(uri,{
-// //     useNewUrlParser:true,
-// //     useUnifiedTopology:true,
-// // }).then(()=>{
-// //     console.log("Connected to Database");
-// // }).catch((err) =>console.log(err));
-
-// module.exports = mongoose;
-
 const mongoose = require("mongoose");
 require('dotenv').config()
 
