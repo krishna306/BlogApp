1983e92879c278d196c223c19e0a07523f75c7f6
krishna306 <krishnakumarkeer5017@gmail.com>
2023-01-07 18:11:50 +0530

    updated version

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-const { verify } = require("jsonwebtoken");
-const { findOne } =require("../models/User");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 const authUser = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = verify(token, process.env.SECRETKEY);
-    const user = await findOne({ _id: decoded._id });
+    const decoded = jwt.verify(token, process.env.SECRETKEY);
+    const user = await User.findOne({ _id: decoded._id });
     if (!user) {
       throw new Error("Please Authenticate");
     }
@@ -15,27 +15,5 @@ const authUser = async (req, res, next) => {
     res.status(401).send({ error: e.message });
   }
 };
-
-// const authAdmin = async(req,res,next)=>{
-//     try{
-//         const token = req.header('Authorization').replace('Bearer ','');
-//         const decoded = jwt.verify(token,process.env.SECRETKEY);
-//         const user = await User.findOne({_id:decoded._id});
-//         if(!user){
-//             throw new Error("Please Athenticate");
-//         }
-//         if(decoded.role === "admin"){
-//             req.token = token;
-//             req.user = user;
-//             next();
-//         }
-//         else {
-//             throw new Error("You are not authorize to access these data");
-//         }
-//     }
-//     catch(e){
-//         res.status(401).send({error: e.message});
-//     }
-// }
 
 module.exports = { authUser };
