8e61b2ce025dc3349e66fb77f1df2057cc76a950
Krishna Kumar <krishnakumarkeer5017@gmail.com>
2026-05-15 01:01:16 +0530

    mongoose errorfix

--- connection.js
+++ connection.js
@@ -1,17 +1,18 @@
-const mongoose = require("mongoose");
-require('dotenv').config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-// mongoose connection for local databse
+dotenv.config();
+
+// mongoose connection for local database
 // mongoose.connect("mongodb://localhost:27017/blogpost")
 // .then(console.log("connected to Database"))
 // .catch((err) => console.log(err));
 // Database connection for mongodb atlas
 const uri = process.env.MongoURL;
-mongoose.connect(uri,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
+mongoose.connect(uri)
+  .then(() => {
     console.log("Connected to Database");
-}).catch((err) =>console.log(err));
+  })
+  .catch((err) => console.log(err));
 
-module.exports = mongoose;
+export default mongoose;
